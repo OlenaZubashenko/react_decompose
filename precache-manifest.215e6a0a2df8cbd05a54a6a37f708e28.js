c0ed41fc952edd6504897e3a0320e25ddced6b29
olenazubashenko <olenazubashenko@gmail.com>
2020-04-22 19:59:09 +0300

    Updates

---
+++ precache-manifest.215e6a0a2df8cbd05a54a6a37f708e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ccae31b4eeb7cd4199ff71ed29dc5c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1dc2f962f2ac80d4408a",
+    "url": "./static/css/main.7e3b1e4f.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc2f962f2ac80d4408a",
+    "url": "./static/js/main.8a623580.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
